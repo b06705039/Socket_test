51892daf216186e5fe840268dc08d81c2c8f1928
ueuesoftware <ueuesoftware@gmail.com>
2021-06-01 23:58:23 +0800

    ok version

--- server_excute.py
+++ server_excute.py
@@ -9,5 +9,5 @@ sent_msg = [ '0102', '0102']
 Line_url = "https://maker.ifttt.com/trigger/DemoTest/with/key/iMHP9IsfpS0DbbDCMJmro16spw7fdOZxl5te3bC2eb6"
 
 
-for i in range(3):
-    Popen(["python3", "http_route.py", socket_port[i], http_port[i], sent_msg[i], Line_url, str(i)])
+for i in range(2):
+    Popen(["python3", "Socket_test/http_route.py", socket_port[i], http_port[i], sent_msg[i], Line_url, str(i)])
